dbc256bdf0f7516b2b12c80233a60434b9a5c5e7
nahom <nahumtefera@gmail.com>
2023-10-20 15:34:43 -0400

    created get query mutation

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,24 @@ import { v } from "convex/values";
 import {mutation, query } from './_generated/server';
 import { Doc, Id } from './_generated/dataModel';
 
+
+export const get = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity) {
+            throw new Error('User not authenticated');
+        }
+
+        // const userId = identity.subject;
+
+        const documents = await ctx.db.query("documents").collect();
+
+        return documents;
+    }
+})
+
+
 export const create = mutation({
     args: {
         title: v.string(), 
@@ -28,3 +46,5 @@ export const create = mutation({
         return document;
     }
 })
+
+
