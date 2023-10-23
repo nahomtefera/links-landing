376c5c98272b7f44600595b31610b893fa170226
nahom <nahumtefera@gmail.com>
2023-10-23 19:19:27 -0400

    added mutation to update content on database

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Cover from "@/components/cover";
+import Editor from "@/components/editor";
 import Toolbar from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react"
+import { useMutation } from "convex/react"
 
 interface DocumentIdPageProps {
     params: {
@@ -19,6 +21,15 @@ const DocumentIdPage = ({
     const document = useQuery(api.documents.getById, {
         documentId: params.documentId
     })
+
+    const update = useMutation(api.documents.update)
+
+    const onChange = (content: string) => {
+        update({
+            id: params.documentId,
+            content
+        })
+    }
 
     if (document === undefined) {
         return (
@@ -47,6 +58,10 @@ const DocumentIdPage = ({
             <Cover url={document.coverImage}/>
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
+                <Editor 
+                    onChange={onChange}
+                    initialContent={document.content}
+                />
             </div>
         </div>
      );
