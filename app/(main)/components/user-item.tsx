9171dea4a34bf9604201622af876b34e60212ddc
nahom <nahumtefera@gmail.com>
2023-11-02 12:03:55 -0400

    fixed import error

--- app/(main)/components/user-item.tsx
+++ app/(main)/components/user-item.tsx
@@ -6,7 +6,7 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdowmMenuLabel,
+    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
